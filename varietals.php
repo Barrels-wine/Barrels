d04e13fb94e7d90ff446f817f3d845fecc4326e7
Suzanne Favot <sfavot@jolicode.com>
2018-08-17 11:41:14 +0200

    rhone

--- varietals.php
+++ varietals.php
@@ -6,7 +6,7 @@ $raw = preg_replace('/\s+/', ' ', $raw);
 $raw = str_replace(' ,', ',', $raw);
 $raw = preg_replace('/,+/', ',', $raw);
 $raw = str_replace(", ", ",\n", $raw);
-$raw = str_replace(['-', ' '], '_', $raw);
+$raw = str_replace(['-', ' ', '’', '\''], '_', $raw);
 
 $raw = str_replace('é', 'e', $raw);
 $raw = str_replace('è', 'e', $raw);
@@ -18,8 +18,6 @@ $raw = str_replace('û', 'u', $raw);
 $raw = str_replace('ô', 'o', $raw);
 $raw = str_replace('â', 'a', $raw);
 $raw = str_replace('ê', 'e', $raw);
-$raw = str_replace('\'', '_', $raw);
-$raw = str_replace('’', '_', $raw);
 
 $raw = strtoupper($raw);
 $raw = 'Varietals::'.$raw;
